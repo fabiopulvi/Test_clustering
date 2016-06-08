df6d42879f250b3e2959679424ca539521df3f41
fabiopulvi <fabio.pulvirenti88@gmail.com>
2016-06-08 18:20:15 +0200

    on my way to complete kmedoid transformation =)

--- PAM/src/KMeans_Ex4a.java
+++ PAM/src/KMeans_Ex4a.java
@@ -4,12 +4,12 @@ import java.util.Random;
 public class KMeans_Ex4a
 {
     private static final int NUM_CLUSTERS = 4;    // Total clusters.
-    private static final int TOTAL_DATA = 100;      // Total data points.
-    static double imbalance =100;
+    private static final int TOTAL_DATA = 10000;      // Total data points.
+    static double imbalance =5000000;
     static double partition_constraint = (double) (imbalance * (TOTAL_DATA+1) / NUM_CLUSTERS);
     static int max_value=10000000;
     static ArrayList<ArrayList<Double>> dataset = new ArrayList<ArrayList<Double>>();
-
+    static int iteration = 0;
     private static final double SAMPLES[][] = new double[][] {{1.0, 1.0},
             {1.5, 2.0},
             {3.0, 4.0},
@@ -17,8 +17,56 @@ public class KMeans_Ex4a
             {3.5, 5.0},
             {4.5, 5.0},
             {3.5, 4.5}};
+/*
+    private int findMedoid(Matrix matrix, Matrix cdata, int cluster, int[] clusterid) {
+        double minDistance = Double.MAX_VALUE;
+        int medoid = -1;
+        for (int row = 0; row < matrix.nRows(); row++) {
+            if (clusterid[row] == cluster) {
+                double distance = metric.getMetric(matrix, cdata, matrix.getWeights(), row, cluster);
+                if (distance < minDistance) {
+                    medoid = row;
+                    minDistance = distance;
+                }
+            }
+        }
+        return medoid;
+    }
 
-
+     * Updates the medoid of one cluster
+	 *
+	 * @param clusterIndex the index of the cluster
+	 * @param clusterMember the indices of the instance members in the cluster
+	 * @return true if the medoid of this cluster is changed and update the m_Medoids;
+	 * 			otherwise return false
+	 */
+    /*
+private boolean updateMedoid(int clusterIndex, ArrayList<Integer> clusterMember) {
+    double BestCost = m_DistanceErrors[clusterIndex];
+    int NewMedoid = m_Medoids[clusterIndex];
+    int ClusterSize = clusterMember.size();
+    for (int i=0; i<ClusterSize; i++) {
+        double CurrentCost = 0;
+        int CurrentMedoid = clusterMember.get(i);
+        for (Integer x : clusterMember) {
+            CurrentCost += m_DistanceFunction.distance(
+                    m_data.instance(CurrentMedoid), m_data.instance(x));
+        }
+        if (CurrentCost < BestCost) {
+            NewMedoid = CurrentMedoid;
+            BestCost = CurrentCost;
+        }
+    }
+    if (NewMedoid == m_Medoids[clusterIndex]) {
+        return false;	// Not changed
+    }
+    else {
+        m_Medoids[clusterIndex] = NewMedoid;
+        m_DistanceErrors[clusterIndex] = BestCost;
+        return true;
+    }
+}
+    */
 
     private static ArrayList<Data> dataSet = new ArrayList<Data>();
     private static ArrayList<Centroid> centroids = new ArrayList<Centroid>();
@@ -27,17 +75,25 @@ public class KMeans_Ex4a
     {
         System.out.println("Centroids initialized at:");
         Random r = new Random();
-        centroids.add(new Centroid(r.nextInt(max_value) , r.nextInt(max_value))); // lowest set.
-        centroids.add(new Centroid(r.nextInt(max_value), r.nextInt(max_value))); // highest set.
-        centroids.add(new Centroid(r.nextInt(max_value) , r.nextInt(max_value))); // lowest set.
-        centroids.add(new Centroid(r.nextInt(max_value), r.nextInt(max_value))); // highest set.
+       // centroids.add(new Centroid(r.nextInt(max_value) , r.nextInt(max_value))); // lowest set.
+       // centroids.add(new Centroid(r.nextInt(max_value), r.nextInt(max_value))); // highest set.
+
+
+        for(int i = 0; i < NUM_CLUSTERS; i++)
+        {
+            centroids.add(new Centroid(r.nextInt(max_value), r.nextInt(max_value)));
+        }
+        /*
         centroids.add(new Centroid(1,1)); // lowest set.
         centroids.add(new Centroid(6,6));
         centroids.add(new Centroid(6,1));
         centroids.add(new Centroid(1,6));// highest set.
-        System.out.println("     (" + centroids.get(0).X() + ", " + centroids.get(0).Y() + ")");
-        System.out.println("     (" + centroids.get(1).X() + ", " + centroids.get(1).Y() + ")");
-        System.out.print("\n");
+        */
+        for(int i = 0; i < NUM_CLUSTERS; i++)
+        {
+            System.out.println("     (" + centroids.get(i).X() + ", " + centroids.get(i).Y() + ")");
+        }
+
 
 
         for (int i=0;i<TOTAL_DATA;i++) {
@@ -65,6 +121,7 @@ public class KMeans_Ex4a
         boolean isStillMoving = true;
         Data newData = null;
         int size=0;
+        int max_iterations=10;
 
         // Add in new data, one at a time, recalculating centroids with each new one. 
       //{
@@ -82,9 +139,12 @@ public class KMeans_Ex4a
                 similarity = sim(newData, centroids.get(i))* (1 - size / partition_constraint);
                 if(similarity > maximum){
                     maximum = similarity;
+                    Random r = new Random();
                     cluster = i;
                     //System.out.println("point:"+newData.X()+","+newData.Y()+", has been assigned to: "+centroids.get(i).X()+","+centroids.get(i).Y());
                 }
+                Random r = new Random();
+                cluster=r.nextInt(NUM_CLUSTERS);
             }
             newData.cluster(cluster);
                 dataSet.add(newData);
@@ -110,14 +170,18 @@ public class KMeans_Ex4a
                 centroids.get(i).X(totalX / totalInCluster);
                 centroids.get(i).Y(totalY / totalInCluster);
                 centroids.get(i).size(totalInCluster);
-                System.out.println("the new centroid is in:"+centroids.get(i).X()+","+centroids.get(i).Y()+" centroid has elements: "+centroids.get(i).size);
+                //System.out.println("the new centroid is in:"+centroids.get(i).X()+","+centroids.get(i).Y()+" centroid has elements: "+centroids.get(i).size);
             }
             centroids.get(i).size(totalInCluster);
         }
-
-        // Now, keep shifting centroids until equilibrium occurs.
-        while(isStillMoving)
+        for(int i = 0; i < NUM_CLUSTERS; i++)
         {
+            System.out.println(" centroid:"+i+"    (" + centroids.get(i).X() + ", " + centroids.get(i).Y() + ")");
+        }
+        // Now, keep shifting centroids until equilibrium occurs.
+
+        while(isStillMoving)
+        {   iteration++;
             System.out.println("assigning the points");
 
 
@@ -167,20 +231,25 @@ public class KMeans_Ex4a
                     }
                 }
                 if(totalInCluster > 0){
-                    System.out.println(+totalX+","+totalInCluster);
-                    System.out.println(+totalX+","+totalInCluster);
+                   // System.out.println(+totalX+","+totalInCluster);
+                   // System.out.println(+totalX+","+totalInCluster);
                     centroids.get(i).X((double) totalX / totalInCluster);
                     centroids.get(i).Y((double) totalY / totalInCluster);
                     centroids.get(i).size(totalInCluster);
-                    System.out.println("the new centroid is in:"+centroids.get(i).X()+","+centroids.get(i).Y()+" centroid has elements: "+centroids.get(i).size);
+                    //System.out.println("the new centroid is in:"+centroids.get(i).X()+","+centroids.get(i).Y()+" centroid has elements: "+centroids.get(i).size);
+                    //System.out.println("the new centroid is in:"+centroids.get(i).X()+","+centroids.get(i).Y()+" centroid has elements: "+centroids.get(i).size);
 
                 }
                 centroids.get(i).size(totalInCluster);
             }
             boolean equal = true;
+            for(int i = 0; i < NUM_CLUSTERS; i++)
+            {
+                System.out.println(" centroid:"+i+"    (" + centroids.get(i).X() + ", " + centroids.get(i).Y() + ") with:"+centroids.get(i).size());
+            }
             for(int i = 0; i < NUM_CLUSTERS; i++) {
                 distance = dist_centroid(centroids_old.get(i), centroids.get(i));
-                System.out.println("Now we will compare the old centroids: "+centroids_old.get(i).X()+","+centroids_old.get(i).Y()+" with the new ones: "+centroids.get(i).X()+","+centroids.get(i).Y());
+                //System.out.println("Now we will compare the old centroids: "+centroids_old.get(i).X()+","+centroids_old.get(i).Y()+" with the new ones: "+centroids.get(i).X()+","+centroids.get(i).Y());
 
                 if (distance!=0) equal=false;
             }
@@ -188,6 +257,10 @@ public class KMeans_Ex4a
 
                 isStillMoving=false;
             }
+            if (iteration==max_iterations) {
+                isStillMoving=false;
+            }
+
 
         }
         return;
@@ -330,7 +403,7 @@ public class KMeans_Ex4a
             for(int j = 0; j < TOTAL_DATA; j++)
             {
                 if(dataSet.get(j).cluster() == i){
-                    System.out.println("     (" + dataSet.get(j).X() + ", " + dataSet.get(j).Y() + ")");
+                    //System.out.println("     (" + dataSet.get(j).X() + ", " + dataSet.get(j).Y() + ")");
                 }
             } // j
             System.out.println();
@@ -342,7 +415,7 @@ public class KMeans_Ex4a
         {
             System.out.println("     (" + centroids.get(i).X() + ", " + centroids.get(i).Y()+" size:"+centroids.get(i).size );
         }
-        System.out.print("\n");
+        System.out.print("iterations: "+iteration+"\n");
         return;
     }
 }
